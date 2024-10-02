f19c8b2044425495b4169264b661e2eaa9c6cdc3
ryyHardy <ryyhardy@gmail.com>
2024-10-02 00:39:36 -0400

    copied bot code from mineflayer documentation

--- bot.py
+++ bot.py
@@ -1,39 +1,49 @@
 from dotenv import load_dotenv
 import os
+import sys
 from javascript import require, On
+
 mineflayer = require("mineflayer")
+pathfinder = require("mineflayer-pathfinder")
 
 load_dotenv()
 
+BOT_USERNAME = "TextMCBot"
+
+
 class BuilderBot:
     def __init__(self) -> None:
-        self.setup_listeners()
         self.API_KEY = os.getenv("OPENAI_APY_KEY")
-    
+        self.bot = mineflayer.createBot({"host": "localhost", "username": BOT_USERNAME})
+        self.bot.loadPlugin(pathfinder.pathfinder)
+        print("Started mineflayer")
+        self.setup_listeners()
+
     def setup_listeners(self):
         @On(self.bot, "spawn")
         def handle_spawn(*args):
-            """
-            Spawns the bot
-            """
-            pass
-        
+            """Spawns the bot"""
+            print("Hello!")
+            self.movements = pathfinder.Movements(self.bot)
+
         @On(self.bot, "chat")
         def on_chat(this, sender, message, *args):
-            """Handles chats
+            """Handles chats"""
+            if sender and (sender != BOT_USERNAME):
+                if "come" in message:
+                    player = self.bot.players[sender]
+                    print("Target", player)
+                    target = player.entity
+                    if not target:
+                        self.bot.chat("I don't see you!")
+                        return
+                    pos = target.position
+                    self.bot.pathfinder.setMovements(self.movements)
+                    self.bot.pathfinder.setGoal(
+                        pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, 1)
+                    )
 
-            :param this: _description_
-            :type this: _type_
-            :param sender: _description_
-            :type sender: _type_
-            :param message: _description_
-            :type message: _type_
-            :param args: _description_
-            :type args: _type_
-            """
-            pass
-        
+        @On(self.bot, "end")
         def on_end(*args):
-            """Ends the bot
-            """
-            pass
+            """Ends the bot"""
+            print("Bot ended", args)
---
+++ main.py
@@ -0,0 +1,9 @@
+from bot import BuilderBot
+
+
+def main():
+    b = BuilderBot()
+
+
+if __name__ == "__main__":
+    main()
