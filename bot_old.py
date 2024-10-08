872fa210d5f4e021b27dbb7c5ca7b7a33f78f3a5
Ryan Hardy <155328211+ryyHardy@users.noreply.github.com>
2024-10-07 23:45:07 -0400

    Removed old bot code

--- bot_old.py
+++
@@ -1,9 +0,0 @@
-            # else: #! Otherwise, get response using the LLM
-            #     completion = self.openai_client.chat.completions.create(
-            #         model="gpt-4o-mini",
-            #         messages=[
-            #             {"role": "system", "content": CHATBOT_ROLE},
-            #             {"role": "user", "content": message}
-            #         ]
-            #     )
-            #     self.bot.chat(completion.choices[0].message.content)
