edf9bd0c18569e4f93d47c59f415c7c497cad5fc
ryyHardy <ryyHardy@gmail.com>
2025-08-24 14:41:56 -0400

    fixed theme system (hopefully)

--- src/frontend/contexts/ThemeContext.tsx
+++ src/frontend/contexts/ThemeContext.tsx
@@ -18,7 +18,7 @@ type ThemeContextType = {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("dark");
 
   useEffect(() => {
     const root = document.documentElement;
